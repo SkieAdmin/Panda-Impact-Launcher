a3c7d0a5ee0b03ea309607c6a47591c67ef6e70c
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2022-10-12 23:21:13 +0800

    Add files via upload

---
+++ Panda Downloader/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Panda_Downloader
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    File.WriteAllText("InstallPath.log", fbd.SelectedPath);
+                    startDownload();
+                    return;
+                }
+                Environment.Exit(0);
+            }
+        }
+
+        private void startDownload()
+        {
+            Thread thread = new Thread(() => {
+                WebClient client = new WebClient();
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri("https://pandatechnology.xyz/Utilities/Core/PandaImpactLauncher.zip"), "./Content.zip");
+            });
+            thread.Start();
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                label2.Text = "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+
+        private void appShortcutToDesktop(string linkName)
+        {
+            string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            using (StreamWriter writer = new StreamWriter(deskDir + "\\" + linkName + ".url"))
+            {
+                string app = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=file:///" + app);
+                writer.WriteLine("IconIndex=0");
+                string icon = app.Replace('\\', '/');
+                writer.WriteLine("IconFile=" + icon);
+            }
+        }
+        async void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate {
+                label2.Text = "Extracting Content";
+            });
+            await Task.Delay(2000);
+            string path = File.ReadAllText("InstallPath.log");
+            ZipFile.ExtractToDirectory("./Content.zip", path);
+            appShortcutToDesktop(path + "\\ModLauncher-Guard.exe");
+            Process.Start(path + "\\ModLauncher-Guard.exe");
+            File.Delete("InstallPath.log");
+            await Task.Delay(2000);
+            this.BeginInvoke((MethodInvoker)delegate {
+                this.Close();
+            });
+        }
+    }
+}
